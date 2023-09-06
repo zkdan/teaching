f81918d40e42fcea43784e7ed593f22ff8ccf979
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2023-09-06 10:04:13 -0400

    animal array

---
+++ animal-array.js
@@ -0,0 +1,31 @@
+const animals = [{
+  "isFavourited": false,
+  "url": "https://placekitten.com/500/350",
+  "alt": "Riley the cat",
+  "id": "01"
+},{
+  "isFavourited": false,
+  "url": " https://placedog.net/400",
+  "alt": "Marley the dog",
+  "id": "02"
+},{
+  "isFavourited": false,
+  "url": "https://placekitten.com/400/300",
+  "alt": "Laveau the cat",
+  "id": "03"
+},{
+  "isFavourited": false,
+  "url": "https://placedog.net/500",
+  "alt": "Nadine the dog",
+  "id": "04"
+},{
+  "isFavourited": false,
+  "url": "https://placekitten.com/300/350",
+  "alt": "Booster the cat",
+  "id": "05"
+},{
+  "isFavourited": false,
+  "url": "https://placekitten.com/300/300",
+  "alt": "Beetle the cat",
+  "id": "06"
+}]
